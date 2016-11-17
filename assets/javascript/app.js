a6083c21b1ce63f79f5b115c3a69c41cacf2812d
Omar Elhadary <Omar Elhadary>
2016-11-17 03:01:50 -0800

    displaying stored data in faq sidebar and cleaned up code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,7 +40,6 @@ $(document).ready(function() {
             buttons.attr("src", locations[i].image)
             buttons.attr("data-city", locations[i].city);
             buttons.attr("data-state", locations[i].state);
-
             buttons.addClass("city-button");
             buttons.html(locations[i].city + ", " + locations[i].state);
             $(".buttons").append(buttons);
@@ -48,37 +47,29 @@ $(document).ready(function() {
     }
     // get Weather API results for pre-selected locations and append to page
     function displayWeather() {
-
-// on the click function, pass to local storage the class of the button you're clicking on
-// if location.href=is the destination page,
-// make an array of the localtion and parseit for the .html page
-// then getlocalstoarge, run ajax call with those values and display at that page
-// var uriList = window.location.href.split('/');
-// var uri = uriList[uriList.length - 1];
-
-// IF PLANTS OPEN MODAL
-// if (uri === 'destinations') {
+        // on the click function, pass to local storage the class of the button you're clicking on
+        // if location.href=is the destination page,
+        // make an array of the localtion and parseit for the .html page
+        // then getlocalstoarge, run ajax call with those values and display at that page
+        // var uriList = window.location.href.split('/');
+        // var uri = uriList[uriList.length - 1];
+        // IF PLANTS OPEN MODAL
+        // if (uri === 'destinations') {
         //stuff
-// }
-
+        // }
         cityName = $(this).data("city");
         stateName = $(this).data("state");
         // alert("cityName: " + cityName + "stateName: " + stateName);
         localStorage.setItem("localStorageCityName", cityName);
         localStorage.setItem("localStorageStateName", stateName);
-
         var weatherDiv = $("<div>");
         var localStorageCityName = localStorage.getItem("localStorageCityName");
         // alert(localStorageCityName);
         var localStorageStateName = localStorage.getItem("localStorageStateName");
         var queryURL = "http://api.wunderground.com/api/5c889a067ba72299/geolookup/conditions/q/" + localStorageStateName + "/" + localStorageCityName + ".json";
         // alert(queryURL);
-
-
         $(document).ready(function($) {
             // alert("almost works");
-
-
             $.ajax({
                 url: queryURL,
                 dataType: "jsonp",
@@ -93,7 +84,6 @@ $(document).ready(function() {
                     var windDirection = parsed_json["current_observation"]["wind_dir"];
                     var weatherIcon = $("<img>");
                     weatherIcon.attr("src", parsed_json["current_observation"]["icon_url"]);
-
                     // var forecast = parsed_json["forecast"]["simpleforecast"]["forecastday"]["date"];
                     // var forecast = parsed_json["forecast"]["txt_forecast"]["forecastday.indexOf[0]"];
                     // prepend the weatherDiv to the page
@@ -107,28 +97,20 @@ $(document).ready(function() {
                     $(".weather-wind-direction").html("Wind Direction: " + windDirection);
                     $(".weather-humidity").html("Humidity: " + humidity);
                     // weatherDiv.append("...Forecast: " + forecast + "...");
-
                     // $(".cities").prepend(weatherDiv);
                     // alert("works");
-
                 },
                 error: function(error) {
                     alert('error; ' + eval(error));
                 }
-
-
             });
-
         });
-
     }
     // get Weather API results for new locations and append to page
     function displayNewWeather() {
         var newWeatherDiv = $("<div>");
         var queryURL = "http://api.wunderground.com/api/5c889a067ba72299/geolookup/conditions/q/" + newState + "/" + newCity + ".json";
-
         $(document).ready(function($) {
-
             $.ajax({
                 url: queryURL,
                 dataType: "jsonp",
@@ -143,7 +125,6 @@ $(document).ready(function() {
                     var windDirection = parsed_json["current_observation"]["wind_dir"];
                     var weatherIcon = $("<img>");
                     weatherIcon.attr("src", parsed_json["current_observation"]["icon_url"]);
-
                     // var forecast = parsed_json["forecast"]["simpleforecast"]["forecastday"]["date"];
                     // var forecast = parsed_json["forecast"]["txt_forecast"]["forecastday.indexOf[0]"];
                     // prepend the weatherDiv to the page
@@ -157,9 +138,6 @@ $(document).ready(function() {
                     $(".weather-wind-direction").html("Wind Direction: " + windDirection);
                     $(".weather-humidity").html("Humidity: " + humidity);
                     // weatherDiv.append("...Forecast: " + forecast + "...");
-
-
-
                     // $(".cities").prepend(newWeatherDiv);
                 },
                 error: function(error) {
@@ -188,42 +166,32 @@ $(document).ready(function() {
     })
     // when user input is added to Firebase, append the stored values to the page
     database.ref("Destinations").on("child_added", function(childSnapshot) {
-        $(".added-destination").append("<tr>+<td>" + "<button>Destination Link</button>" + "<td>" + childSnapshot.val().newCity + "<td>" + childSnapshot.val().newState);
+        $(".added-destination").append("<tr>+<td>" + "<button class='destination-stored-API'>Destination Link</button>" + "<td>" + childSnapshot.val().newCity + "<td>" + childSnapshot.val().newState);
     });
     // create on click event handler to display the weather if any of the topic buttons are clicked
-    $(document).on('click', ".city-button", "#destination-link", displayWeather);
+    $(document).on('click', ".city-button", displayWeather);
+    // create on click event handler to display weather if the destination link is clicked
+    $(document).on('click', ".destination-stored-API", displayNewWeather);
     // function to navigate to destinations page
     function destinationsPage() {
         // $(".buttons").attr('href','destinations.html');
         // buttons.attr("href", "destinations.html");
-
-
         // for (var j = 0; j < locations.length; j++) {
         //     cityName = locations[j].city;
         // alert(cityName);
         // stateName = locations[j].state;
-
         //     cityName = $(this).data("city");
         // stateName = $(this).data("state");
         // alert("cityName: " + cityName + "stateName: " + stateName);
         //     localStorage.setItem("localStorageCityName", cityName);
         //     localStorage.setItem("localStorageStateName", stateName);
-
-
-
         // }
-
         window.location.href = "destinations.html";
-
-
-
-
         displayWeather();
     }
     // run function to show the topics buttons
     showCity();
-
-// Get the search modal
+    // Get the search modal
     var searchModal = document.getElementById("search-modal");
     var searchButton = document.getElementById("search-button");
     var searchSpan = document.getElementsByClassName("search-close")[0];
@@ -238,202 +206,143 @@ $(document).ready(function() {
             searchModal.style.display = "none";
         }
     }
-
-
-// create on click event handler to take login username and password and store in firebase
+    // create on click event handler to take login username and password and store in firebase
     $("#logInButton").on("click", function() {
         var loginUserName = $("#username").val().trim();
-
         var loginPassword = $("#password").val().trim();
-
         database.ref("login").push({
             loginUserName: loginUserName,
             loginPassword: loginPassword,
         })
-
         $("#username").val("");
         $("#password").val("");
         // prevents page from refreshing when user submits input
         return false;
     })
-
-// create on click event handler to take Sign Up username, password and email and store in firebase
+    // create on click event handler to take Sign Up username, password and email and store in firebase
     $("#signUpButton").on("click", function() {
         var signUpUserName = $("#username1").val().trim();
-
         var signUpPassword = $("#password1").val().trim();
-
         var signUpEmail = $("#e-mail1").val().trim();
         var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-        if(signUpEmail.value.match(mailformat)) {
+        if (signUpEmail.value.match(mailformat)) {
             database.ref("Sign Up").push({
                 signUpUserName: signUpUserName,
                 signUpPassword: signUpPassword,
                 signUpEmail: signUpEmail,
             })
-
             $("#username1").val("");
             $("#password1").val("");
             $("#e-mail1").val("");
+            $(".incorrect-email-alert").html("");
             // prevents page from refreshing when user submits input
             return false;
         } else {
             $(".incorrect-email-alert").html("Enter a valid email.");
         }
     })
-
-
-
-
-// MapBox API code
-// ************************************************************************************
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2xpY2t0b25lIiwiYSI6ImNpdmxub2c5ZTA1N2MyenF0OWZweXo2Y3MifQ.-NOxV9teamyijL1FWXSMmA';
-
-var map = L.mapbox.map('map', 'mapbox.streets', {
-    zoomControl: false
-}).setView([40, -74.50], 9);
-
-// move the attribution control out of the way
-map.attributionControl.setPosition('bottomleft');
-
-// create the initial directions object, from which the layer
-// and inputs will pull data.
-var directions = L.mapbox.directions();
-
-var directionsLayer = L.mapbox.directions.layer(directions)
-    .addTo(map);
-
-var directionsInputControl = L.mapbox.directions.inputControl('inputs', directions)
-    .addTo(map);
-
-var directionsErrorsControl = L.mapbox.directions.errorsControl('errors', directions)
-    .addTo(map);
-
-var directionsRoutesControl = L.mapbox.directions.routesControl('routes', directions)
-    .addTo(map);
-
-var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions)
-    .addTo(map);
-
-// ************************************************************************************
-
-
-
-// Justin's functionality code //
-
-var faqModal = document.getElementById('faqModal');
-
-var faqSpan = document.getElementsByClassName("close")[2];
-
-$("#faq").on("click", function(){
-    $("#faqModal").css("display", "block");
-    $("#signupModal").css("display", "none");
-    $("#loginModal").css("display", "none");
-    $(".navigation-bar").fadeOut(0200);
-    $("html").css("width", "70%")
-})
-
-faqSpan.onclick = function() {
-    $("#faqModal").css("display", "none")
-    $(".navigation-bar").fadeIn();
-    $("html").css("width", "100%")
-}
-
-$(window).on("click", function(event) {
-    if (event.target == faqModal) {
-        faqModal.style.display = "none";
+    // MapBox API code
+    // ************************************************************************************
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic2xpY2t0b25lIiwiYSI6ImNpdmxub2c5ZTA1N2MyenF0OWZweXo2Y3MifQ.-NOxV9teamyijL1FWXSMmA';
+    var map = L.mapbox.map('map', 'mapbox.streets', {
+        zoomControl: false
+    }).setView([40, -74.50], 9);
+    // move the attribution control out of the way
+    map.attributionControl.setPosition('bottomleft');
+    // create the initial directions object, from which the layer
+    // and inputs will pull data.
+    var directions = L.mapbox.directions();
+    var directionsLayer = L.mapbox.directions.layer(directions).addTo(map);
+    var directionsInputControl = L.mapbox.directions.inputControl('inputs', directions).addTo(map);
+    var directionsErrorsControl = L.mapbox.directions.errorsControl('errors', directions).addTo(map);
+    var directionsRoutesControl = L.mapbox.directions.routesControl('routes', directions).addTo(map);
+    var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions).addTo(map);
+    // ************************************************************************************
+    // Justin's functionality code //
+    var faqModal = document.getElementById('faqModal');
+    var faqSpan = document.getElementsByClassName("close")[2];
+    $("#faq").on("click", function() {
+        $("#faqModal").css("display", "block");
+        $("#signupModal").css("display", "none");
+        $("#loginModal").css("display", "none");
+        $(".navigation-bar").fadeOut(0200);
+        $("html").css("width", "70%")
+    })
+    faqSpan.onclick = function() {
+        $("#faqModal").css("display", "none")
         $(".navigation-bar").fadeIn();
-        $("html").css("width", "100%");
+        $("html").css("width", "100%")
     }
-});
-
-var signModal = document.getElementById('signupModal');
-
-var signSpan = document.getElementsByClassName("close")[1];
-
-$("#sign-up").on("click", function(){
-    $("#signupModal").css("display", "block");
-    $("#loginModal").css("display", "none");
-    $("#faqModal").css("display", "none");
-    $("html").css("width", "100%")
-    $(".navigation-bar").fadeOut(0200);
-});
-
-signSpan.onclick = function() {
-    $("#signupModal").css("display", "none");
-    $(".navigation-bar").fadeIn();
-}
-
-$(window).on("click", function(event) {
-    if (event.target == signModal) {
-        signModal.style.display = "none";
+    $(window).on("click", function(event) {
+        if (event.target == faqModal) {
+            faqModal.style.display = "none";
+            $(".navigation-bar").fadeIn();
+            $("html").css("width", "100%");
+        }
+    });
+    var signModal = document.getElementById('signupModal');
+    var signSpan = document.getElementsByClassName("close")[1];
+    $("#sign-up").on("click", function() {
+        $("#signupModal").css("display", "block");
+        $("#loginModal").css("display", "none");
+        $("#faqModal").css("display", "none");
+        $("html").css("width", "100%")
+        $(".navigation-bar").fadeOut(0200);
+    });
+    signSpan.onclick = function() {
+        $("#signupModal").css("display", "none");
         $(".navigation-bar").fadeIn();
     }
-});
-
-var modal = document.getElementById('loginModal');
-
-var span = document.getElementsByClassName("close")[0];
-
-$("#log-in").on("click", function(){
-    $("#loginModal").css("display", "block");
-    $("#signupModal").css("display", "none");
-    $("#faqModal").css("display", "none");
-    $("html").css("width", "100%")
-    $(".navigation-bar").fadeOut(0200);
-})
-
-span.onclick = function() {
-    $("#loginModal").css("display", "none");
-    $(".input-name").empty();
-    $(".navigation-bar").fadeIn();
-}
-
-$(window).on("click", function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    $(window).on("click", function(event) {
+        if (event.target == signModal) {
+            signModal.style.display = "none";
+            $(".navigation-bar").fadeIn();
+        }
+    });
+    var modal = document.getElementById('loginModal');
+    var span = document.getElementsByClassName("close")[0];
+    $("#log-in").on("click", function() {
+        $("#loginModal").css("display", "block");
+        $("#signupModal").css("display", "none");
+        $("#faqModal").css("display", "none");
+        $("html").css("width", "100%")
+        $(".navigation-bar").fadeOut(0200);
+    })
+    span.onclick = function() {
+        $("#loginModal").css("display", "none");
+        $(".input-name").empty();
         $(".navigation-bar").fadeIn();
     }
-});
-
-var navModal = document.getElementById('navModal');
-
-var span = document.getElementsByClassName("close")[3];
-
-$("#nav-toggle").on("click", function(){
-    $("#navModal").css("display", "block");
-    $(".header").css("margin-top", "270px");
-    $(".container-space").css("margin-top", "270px");
-    $(".navigation-bar").fadeOut(0200);
-})
-
-$(window).on("click", function(event) {
-    if (event.target == navModal) {
-        navModal.style.display = "none";
+    $(window).on("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            $(".navigation-bar").fadeIn();
+        }
+    });
+    var navModal = document.getElementById('navModal');
+    var span = document.getElementsByClassName("close")[3];
+    $("#nav-toggle").on("click", function() {
+        $("#navModal").css("display", "block");
+        $(".header").css("margin-top", "270px");
+        $(".container-space").css("margin-top", "270px");
+        $(".navigation-bar").fadeOut(0200);
+    })
+    $(window).on("click", function(event) {
+        if (event.target == navModal) {
+            navModal.style.display = "none";
+            $(".navigation-bar").fadeIn();
+            $(".header").css("margin-top", "70px");
+            $(".container-space").css("margin-top", "40px");
+        }
+    });
+    $("#close").on("click", function() {
+        $("#navModal").css("display", "none");
         $(".navigation-bar").fadeIn();
         $(".header").css("margin-top", "70px");
         $(".container-space").css("margin-top", "40px");
-    }
-});
-
-
-$("#close").on("click", function() {
-    $("#navModal").css("display", "none");
-    $(".navigation-bar").fadeIn();
-    $(".header").css("margin-top", "70px");
-    $(".container-space").css("margin-top", "40px");
-});
-
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-    $('#slideshow > div:first')
-        .fadeOut(0800)
-        .next()
-        .fadeIn(0800)
-        .end()
-        .appendTo('#slideshow');
-},  2500);
-
+    });
+    $("#slideshow > div:gt(0)").hide();
+    setInterval(function() {
+        $('#slideshow > div:first').fadeOut(0800).next().fadeIn(0800).end().appendTo('#slideshow');
+    }, 2500);
 });
